c4a41295e32f560ef138b3caa62def1832f91f06
Omarmagdy66 <omarmagdy.omar000@gmail.com>
2024-12-13 13:29:49 +0200

    models and Create database and some function

---
+++ Project Pl3/Database.fs
@@ -0,0 +1,21 @@
+﻿namespace LibraryManagementSystem
+
+open System
+open System.Data.SqlClient
+
+module Database =
+    //let connectionString = "Server=db11009.public.databaseasp.net; Database=db11009; User Id=db11009; Password=7Rj=?i2XY-t5; Encrypt=True; TrustServerCertificate=True; MultipleActiveResultSets=True;"
+    let connectionString = "Server=.;Database=LibraryManagementSystem;Trusted_Connection=True;";
+
+    let createTables() =
+        use conn = new SqlConnection(connectionString)
+        conn.Open()
+        
+    let addBook (title: string) (author: string) (genre: string) =
+        use conn = new SqlConnection(connectionString)
+        conn.Open()
+        use cmd = new SqlCommand("INSERT INTO Books (Title, Author, Gender, Status) VALUES (@Title, @Author, @Gender, 'Available')", conn)
+        cmd.Parameters.AddWithValue("@Title", title) |> ignore
+        cmd.Parameters.AddWithValue("@Author", author) |> ignore
+        cmd.Parameters.AddWithValue("@Gender", genre) |> ignore
+        cmd.ExecuteNonQuery() |> ignore
---
+++ Project Pl3/Models.fs
@@ -0,0 +1,14 @@
+﻿namespace LibraryManagementSystem
+
+// Domain Types
+type BookStatus = 
+    | Available 
+    | Borrowed of borrowDate:System.DateTime * borrower:string
+
+type Book = {
+    Id: int
+    Title: string
+    Author: string
+    Gender: string
+    Status: BookStatus
+}
