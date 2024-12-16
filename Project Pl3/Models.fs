53e40bcf9f482ecb99c70b00003ef1d64a19701b
omarsabri125 <104151494+omarsabri125@users.noreply.github.com>
2024-12-16 10:04:10 +0200

    Update Models.fs

--- Project Pl3/Models.fs
+++ Project Pl3/Models.fs
@@ -4,10 +4,3 @@ type BookStatus =
     | Available 
     | Borrowed of borrowDate:System.DateTime * borrower:string
 
-type Book = {
-    Id: int
-    Title: string
-    Author: string
-    Gender: string
-    Status: BookStatus
-}
